c13966deab26e9ba6df0dfe4b4299dfe44596806
kaia <fedex0329l@gmail.com>
2025-07-04 21:01:55 +0800

    push

--- obj/Debug/net8.0/InventorySyetem.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventorySyetem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySyetem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166f81488756762253fc1c8775b8c14c5f6262b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80a395e28642fca5ee2e7d46f6e39aa085efc9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySyetem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySyetem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
